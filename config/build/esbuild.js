d4d590c4305fc111c0b840cf6a3a435280893194
Paul <64870518+woodcock3@users.noreply.github.com>
2023-01-07 19:26:26 +0000

    Update esbuild.js

--- config/build/esbuild.js
+++ config/build/esbuild.js
@@ -3,7 +3,7 @@ const esbuild = require('esbuild');
 const glob = require('glob-all'); // to enable * glob pattern in esbuild
 const isProd = process.env.ELEVENTY_ENV === 'prod' ? true : false;
 const { solidPlugin } = require('esbuild-plugin-solid');
-const { sveltePlugin } = require('esbuild-svelte');
+const sveltePlugin = require('esbuild-svelte');
 const manifestPlugin = require('esbuild-plugin-manifest');
 const { http, default_schemes } = require('@hyrious/esbuild-plugin-http');
 const fs = require('fs');
