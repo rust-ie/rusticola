20430ecf232958ba3806cfa37497918bf325f7bd
Paul <64870518+woodcock3@users.noreply.github.com>
2023-01-07 19:42:44 +0000

    Update solidify.js

--- config/shortcodes/solidify.js
+++ config/shortcodes/solidify.js
@@ -28,30 +28,10 @@ module.exports = async (code, filename, bundled) => {
     ],
     minify: isProd,
     target: isProd ? 'es6' : 'esnext'
-  }),
-
-  // Cjs version
-  await esbuild.build({
-    entryPoints: glob.sync(['solid-*.jsx']),
-    entryNames: '[name]',
-    outdir: './docs/assets/app',
-    bundle: bundleJsx,
-    format: 'cjs',
-    plugins: [
-      http({
-        filter: (url) => true,
-        schemes: { default_schemes },
-        cache: new Map()
-      }),
-      solidPlugin()
-    ],
-    minify: isProd,
-    target: isProd ? 'es6' : 'esnext'
   }).catch((err) => {
     console.error(err);
     process.exitCode = 1;
   })
-  const solidifyEsm= await fsPromises.readFile('./docs/assets/app/solid-' + filename + '.mjs', 'utf8');
-  const solidifyCjs= await fsPromises.readFile('./docs/assets/app/solid-' + filename + '.js', 'utf8');
-  return `<script type="module">${solidifyEsm}</script><script nomodule>${solidifyCjs}</script>`;
+  const solidifyJsx= await fsPromises.readFile('./docs/assets/app/solid-' + filename + '.js', 'utf8');
+  return `<script type="module">${solidifyJsx}</script>`;
 };
